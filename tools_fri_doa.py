397a5c06f26ba694584b141d228bff736f03bf58
Hanjie Pan <pan@icdhcp-1-074.epfl.ch>
2016-05-13 17:09:56 +0200

    more functions in building G matrix added

--- tools_fri_doa.py
+++ tools_fri_doa.py
@@ -9,22 +9,25 @@ import scipy.optimize
 from functools import partial
 from joblib import Parallel, delayed
 import os
-# from mayavi import mlab
+
 if os.environ.get('DISPLAY') is None:
     import matplotlib
+
     matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 from matplotlib import rcParams
+
 # for latex rendering
 os.environ['PATH'] = os.environ['PATH'] + ':/usr/texbin' + ':/opt/local/bin' + ':/Library/TeX/texbin/'
 rcParams['text.usetex'] = True
 rcParams['text.latex.unicode'] = True
 rcParams['text.latex.preamble'] = [r"\usepackage{bm}"]
 
+
 def great_circ_dist(r, theta1, phi1, theta2, phi2):
     """
-    calculate
+    calculate great circle distance for points located on a sphere
     :param r: radius of the sphere
     :param theta1: colatitude of point 1
     :param phi1: azimuth of point 1
@@ -32,10 +35,6 @@ def great_circ_dist(r, theta1, phi1, theta2, phi2):
     :param phi2: azimuth of point 2
     :return: great-circle distance
     """
-    # theta1 = theta1.flatten('F')
-    # phi1 = phi1.flatten('F')
-    # theta2 = theta2.flatten('F')
-    # phi2 = phi2.flatten('F')
     dphi = np.abs(phi1 - phi2)
     dist = r * np.arctan2(np.sqrt((np.sin(theta2) * np.sin(dphi)) ** 2 +
                                   (np.sin(theta1) * np.cos(theta2) -
@@ -97,7 +96,7 @@ def sph_distance(r, theta1, phi1, theta2, phi2):
 def sph_gen_diracs_param(K, positive_amp=True,
                          log_normal_amp=False,
                          semisphere=True,
-                         save_param=False):
+                         save_param=True):
     """
     Randomly generate Diracs' parameters.
     :param K: number of Diracs
@@ -141,6 +140,8 @@ def sph_gen_diracs_param(K, positive_amp=True,
 
     time_stamp = datetime.datetime.now().strftime('%d-%m_%H_%M')
     if save_param:
+        if not os.path.exists('./data/'):
+            os.makedirs('./data/')
         file_name = './data/sph_Dirac_' + time_stamp + '.npz'
         np.savez(file_name, alpha_ks=alpha_ks, theta_ks=theta_ks,
                  phi_ks=phi_ks, K=K, time_stamp=time_stamp)
@@ -174,3 +175,168 @@ def sph2cart(r, theta, phi):
     y = r_sin_theta * np.sin(phi)
     z = r * np.cos(theta)
     return x, y, z
+
+
+def sph_gen_mic_array(radius_array, num_mic_per_band=1, num_bands=1,
+                      max_ratio_omega=1, save_layout=True):
+    """
+    generate microphone array layout.
+    :param radius_array: radius of the microphone array
+    :param num_mic_per_band: number of microphones
+    :param num_bands: number of narrow bands considered
+    :param max_ratio_omega: we normalise the mid-band frequencies w.r.t.
+            the narraow band that has the lowest frequencies. The ratio here
+            specifies the highest mid-band frequency.
+    :param save_layout: whether to save the microphone array layout or not
+    :return:
+    """
+    # TODO: adapt to the realistic setting later
+    # assume the array is located on a sphere with radius that is
+    # approximated 20 times the radius of stations (almost a flat surface)
+    radius_sph = 20. * radius_array
+    pos_array_norm = np.linspace(0, radius_array, num=num_mic_per_band, dtype=float)
+    pos_array_angle = np.linspace(0, 12. * np.pi, num=num_mic_per_band, dtype=float)
+
+    pos_mic_x = pos_array_norm * np.cos(pos_array_angle)
+    pos_mic_y = pos_array_norm * np.sin(pos_array_angle)
+
+    phi_mic = np.arctan2(pos_mic_y, pos_mic_x)
+    theta_mic = np.arcsin(np.sqrt(pos_mic_x ** 2 + pos_mic_y ** 2) / radius_sph)
+
+    p_mic0_x, p_mic0_y, p_mic0_z = sph2cart(radius_sph, theta_mic, phi_mic)
+    # reshape to use broadcasting
+    p_mic0_x = np.reshape(p_mic0_x, (-1, 1), order='F')
+    p_mic0_y = np.reshape(p_mic0_y, (-1, 1), order='F')
+    p_mic0_z = np.reshape(p_mic0_z, (-1, 1), order='F')
+
+    # mid-band frequencies of different sub-bands
+    mid_band_freq = np.random.rand(1, num_bands) * (max_ratio_omega - 1) + 1
+
+    p_mic_x = p_mic0_x * mid_band_freq
+    p_mic_y = p_mic0_y * mid_band_freq
+    p_mic_z = p_mic0_z * mid_band_freq
+
+    layout_time_stamp = datetime.datetime.now().strftime('%d-%m')
+    if save_layout:
+        directory = './data/'
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        file_name = directory + 'mic_layout_' + layout_time_stamp + '.npz'
+        np.savez(file_name, p_mic_x=p_mic_x, p_mic_y=p_mic_y, p_mic_z=p_mic_z,
+                 layout_time_stamp=layout_time_stamp)
+    return p_mic_x, p_mic_y, p_mic_z, layout_time_stamp
+
+
+def load_mic_array_param(file_name):
+    """
+    load stored microphone array parameters
+    :param file_name: file that stored these parameters
+    :return:
+    """
+    stored_param = np.load(file_name)
+    p_mic_x = stored_param['p_mic_x']
+    p_mic_y = stored_param['p_mic_y']
+    p_mic_z = stored_param['p_mic_z']
+    layout_time_stamp = stored_param['layout_time_stamp'].tostring()
+    return p_mic_x, p_mic_y, p_mic_z, layout_time_stamp
+
+
+def sph_mtx_freq2visibility(L, p_mic_x, p_mic_y, p_mic_z):
+    """
+    build the linear mapping matrix from the spherical harmonics to the measured visibility.
+    :param L: the maximum degree of spherical harmonics
+    :param p_mic_x: a num_mic x num_bands matrix that contains microphones' x coordinates
+    :param p_mic_y: a num_mic y num_bands matrix that contains microphones' x coordinates
+    :param p_mic_z: a num_mic z num_bands matrix that contains microphones' x coordinates
+    :return:
+    """
+    num_mic_per_band, num_bands = p_mic_x.shape
+    m_grid, l_grid = np.meshgrid(np.arange(-L, L + 1, step=1, dtype=int),
+                                 np.arange(0, L + 1, step=1, dtype=int))
+    m_grid = np.reshape(vec_from_low_tri_col_by_col(m_grid), (1, -1), order='F')
+    l_grid = np.reshape(vec_from_low_tri_col_by_col(l_grid), (1, -1), order='F')
+    # parallel implemnetation (over all the subands at once)
+    # TODO: change the G_lst if needed
+    partial_sph_mtx_inner = partial(sph_mtx_freq2visibility_inner, L=L, m_grid=m_grid, l_grid=l_grid)
+    G_lst = Parallel(n_jobs=-1, backend='threading')(delayed(partial_sph_mtx_inner)(p_mic_x[:, band_loop],
+                                                                                    p_mic_y[:, band_loop],
+                                                                                    p_mic_z[:, band_loop])
+                                                     for band_loop in xrange(num_bands))
+    return G_lst
+
+
+def sph_mtx_freq2visibility_inner(p_mic_x_loop, p_mic_y_loop, p_mic_z_loop, L, m_grid, l_grid):
+    """
+    inner loop for the function sph_mtx_freq2visibility
+    :param band_loop: the number of the sub-bands considered
+    :param p_mic_x_loop: a vector that contains the microphone x-coordinates (multiplied by mid-band frequency)
+    :param p_mic_y_loop: a vector that contains the microphone y-coordinates (multiplied by mid-band frequency)
+    :param p_mic_z_loop: a vector that contains the microphone z-coordinates (multiplied by mid-band frequency)
+    :param L: maximum degree of the spherical harmonics
+    :param m_grid: m indices for evaluating the spherical bessel functions and spherical harmonics
+    :param l_grid: l indices for evaluating the spherical bessel functions and spherical harmonics
+    :return:
+    """
+    num_mic_per_band = p_mic_x_loop.size
+    ells = np.arange(L + 1, dtype=float)[np.newaxis, :]
+    G_blk = np.zeros((num_mic_per_band - 1, (L + 1) ** 2), dtype=complex, order='C')
+    count_G_band = 0
+    for q in xrange(num_mic_per_band):
+        p_x_outer = p_mic_x_loop[q]
+        p_y_outer = p_mic_y_loop[q]
+        p_z_outer = p_mic_z_loop[q]
+        for qp in xrange(num_mic_per_band):
+            if not q == qp:
+                p_x_qqp = p_x_outer - p_mic_x_loop[qp]
+                p_y_qqp = p_y_outer - p_mic_y_loop[qp]
+                p_z_qqp = p_z_outer - p_mic_z_loop[qp]
+                norm_p_qqp = np.sqrt(p_x_qqp ** 2 + p_y_qqp ** 2 + p_z_qqp ** 2)
+                # compute bessel function up to order L
+                sph_bessel_ells = (-1j) ** ells * sp.special.jv(ells + 0.5, norm_p_qqp) / np.sqrt(norm_p_qqp)
+
+                theta_qqp = np.arccos(p_z_qqp / norm_p_qqp)
+                phi_qqp = np.arctan2(p_y_qqp, p_x_qqp)
+                # here l_grid and m_grid is assumed to be a row vector
+                G_blk[count_G_band, :] = sph_bessel_ells[l_grid.squeeze()] * \
+                                         sph_harm_ufnc(l_grid, m_grid, theta_qqp, phi_qqp)
+                count_G_band += 1
+    return G_blk * (2 * np.pi) ** 1.5
+
+
+def vec_from_low_tri_col_by_col(mtx):
+    """
+    extract the data from the matrix, which is of the form:
+
+        0 0 0 x 0 0 0
+        0 0 x x x 0 0
+        0 x x x x x 0
+        x x x x x x x
+
+    :param mtx: the matrix to extract the data from
+    :return: data vector
+    """
+    L = mtx.shape[0] - 1
+    assert mtx.shape[1] == 2 * L + 1
+    vec = np.zeros((L + 1) ** 2, dtype=mtx.dtype)
+    bg_idx = 0
+    for loop in xrange(2 * L + 1):
+        if loop <= L:
+            vec_len_loop = loop + 1
+        else:
+            vec_len_loop = 2 * L + 1 - loop
+        vec[bg_idx:bg_idx + vec_len_loop] = mtx[-vec_len_loop:, loop]
+        bg_idx += vec_len_loop
+    return vec
+
+
+def sph_harm_ufnc(l, m, theta, phi):
+    """
+    compute spherical harmonics with the built-in sph_harm function.
+    The difference is the name of input angle names as well as the normlisation factor.
+    :param l: degree of spherical harmonics
+    :param m: order of the spherical harmonics
+    :param theta: co-latitude
+    :param phi: azimuth
+    :return:
+    """
+    return (-1) ** m * sp.special.sph_harm(m, l, phi, theta)
