fa0cbd53e045e39b24e93eb5a0c216cdfdcc29d2
Robin Scheibler <fakufaku@gmail.com>
2016-06-16 17:35:54 +0200

    Script with TD signal and STFT working.

--- figure_RMSE_wn.py
+++ figure_RMSE_wn.py
@@ -29,7 +29,8 @@ if __name__ == '__main__':
 
     # parameters setup
     fs = 8000  # sampling frequency in Hz
-    SNR = 0  # SNR for the received signal at microphones in [dB]
+    SNR = 5  # SNR for the received signal at microphones in [dB]
+    speed_sound = pra.constants.get('c')
 
     K = 5           # Real number of sources
     K_est = 5       # Number of sources to estimate
@@ -38,8 +39,8 @@ if __name__ == '__main__':
     # algorithm parameters
     stop_cri = 'max_iter'  # can be 'mse' or 'max_iter'
     num_snapshot = 256  # number of snapshots used to estimate the covariance matrix
-    fft_size = 128      # number of FFT bins
-    fc = 2000.
+    fft_size = 1024      # number of FFT bins
+    fc = 500.
     fft_bins = [int(fc/fs*fft_size)]
     M = 15  # Maximum Fourier coefficient index (-M to M), K_est <= M <= num_mic*(num_mic - 1) / 2
 
@@ -50,41 +51,22 @@ if __name__ == '__main__':
     #----------------------------
     # Generate all necessary signals
 
-    # generate source parameters at random
-    '''
-    alpha_ks, phi_ks, time_stamp = \
-        gen_diracs_param(K, positive_amp=True, log_normal_amp=False,
-                         semicircle=False, save_param=save_param)
-    '''
-
     # Generate Diracs at random
     phi_ks = 2 * np.pi * np.random.uniform(size=(K))
     #phi_ks = [np.pi/4.]
-    power_ks = 10**(SNR/10)*np.ones(K)
-    #power_ks = [1.]
-    alpha_ks = power_ks #* np.random.normal(size=(K))**2
+    power_ks = 10**(SNR/10)
+    alpha_ks = power_ks * np.random.normal(size=(K))**2
 
     # generate microphone array layout
-    fraction =0.1
-    radius_array =  2000 * 343. / (2*np.pi*fc)  # (2pi * radius_array) compared with the wavelength
+    radius_array =  2. * speed_sound / fc  # (2pi * radius_array) compared with the wavelength
 
     # we would like gradually to switch to our "standardized" functions
     R = pra.spiral_2D_array([0,0], num_mic, radius=radius_array, divi=7, angle=0)
     #R = pra.linear2DArray([0,0], num_mic, 0, radius_array)
     array = pra.Beamformer(R, fs)
-    print R
 
     # Generate the impulse responses for the array and source directions
     ir = gen_far_field_ir([phi_ks], [alpha_ks], R, fs)
-
-    '''
-    plt.figure()
-    for k in range(K):
-        for m in range(num_mic):
-            plt.subplot(K, num_mic, k*num_mic + m + 1)
-            plt.plot(ir[k,m])
-    plt.show()
-    '''
 
     # Now generate some noise
     x = (np.random.normal(size=(num_snapshot*fft_size, K)) * np.sqrt(alpha_ks)).T
@@ -96,8 +78,7 @@ if __name__ == '__main__':
             y[mic] += fftconvolve(ir[src,mic], x[src])
 
     # Add noise to the signals
-    #y_noisy = y + np.random.normal(size=y.shape)
-    y_noisy = y
+    y_noisy = y + np.random.normal(size=y.shape)
 
     # Now do the short time Fourier transform
     # The resulting signal is M x fft_size/2+1 x number of frames
@@ -106,21 +87,7 @@ if __name__ == '__main__':
             )
 
     # Estimate the covariance matrix
-    print Y.shape
     cov_mat = np.dot(Y[:,fft_bins[0],:], np.conj(Y[:,fft_bins[0],:].T))/Y.shape[2]/fft_size
-    print np.angle(cov_mat)/(2.*np.pi*fraction*np.cos(phi_ks[0]))
-    '''
-    plt.matshow(np.angle(cov_mat))
-    plt.show()
-    '''
-
-    # plot received planewaves
-    '''
-    mic_count = 0  # signals at which microphone to plot
-    file_name = fig_dir + 'planewave_mic{0}_SNR_{1:.0f}dB.pdf'.format(repr(mic_count), SNR)
-    plt_planewave(y_mic_noiseless, y_mic_noisy, mic=mic_count,
-                  save_fig=save_fig, file_name=file_name, SNR=SNR)
-    '''
 
     # extract off-diagonal entries
     visi_noisy = extract_off_diag(cov_mat)
@@ -134,7 +101,7 @@ if __name__ == '__main__':
 
     # plot dirty image based on the measured visibilities
     phi_plt = np.linspace(0, 2 * np.pi, num=300, dtype=float)
-    dirty_img = gen_dirty_img(visi_noisy, R[0,:], R[1,:], phi_plt)
+    dirty_img = gen_dirty_img(visi_noisy, R[0,:], R[1,:], 2*np.pi*fc, speed_sound, phi_plt)
 
     # reconstruct point sources with FRI
     max_ini = 50  # maximum number of random initialisation
@@ -142,7 +109,7 @@ if __name__ == '__main__':
     noise_level = 1
     # tic = time.time()
     phik_recon, alphak_recon = \
-            pt_src_recon(visi_noisy, R[0,:], R[1,:], K_est, M, noise_level,
+            pt_src_recon(visi_noisy, R[0,:], R[1,:], 2*np.pi*fc, speed_sound, K_est, M, noise_level,
                      max_ini, stop_cri, update_G=True, G_iter=5, verbose=False)
     # toc = time.time()
     # print(toc - tic)
