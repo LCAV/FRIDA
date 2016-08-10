Fixed SNR computation bug.

Experiment/distances.py
@@ -3,47 +3,43 @@ import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
+from scipy import linalg as la
 
-def classical_mds(D, dim=3):
+import theaudioexperimentalist as tae
 
-    # Apply MDS algorithm for denoising
-    n = D.shape[0]
-    J = np.eye(n) - np.ones((n,n))/float(n)
-    G = -0.5*np.dot(J, np.dot(D, J))
-
-    s, U = np.linalg.eig(G)
-
-    # we need to sort the eigenvalues in decreasing order
-    s = np.real(s)
-    o = np.argsort(s)
-    s = s[o[::-1]]
-    U = U[:,o[::-1]]
-
-    S = np.diag(s)[0:dim,:]
-    return np.dot(np.sqrt(S),U.T)
-
+# The collection of markers where distances were measured
 labels = ['11', '7', '5', '3', '13', '8', '4', '14', 'FPGA', 'BBB']
+# The Euclidean distance matrix. Unit is squared meters
 EDM = np.array(
-      [ [ 0, 0.79, 1.63, 2.42, 2.82, 3.55, 2.44, 2.87, 2.22, 1.46 ],
-        [ 0.79, 0, 1.45, 2.32, 2.49, 3.67, 2.32, 2.54, 1.92, 1.35 ],
-        [ 1.63, 1.45, 0, 1.92, 2.09, 4.02, 3.48, 3.66, 2.50, 1.68 ],
-        [ 2.42, 2.32, 1.92, 0, 0.86, 2.43, 2.92, 3.14, 1.56, 1.14 ],
-        [ 2.82, 2.49, 2.09, 0.86, 0, 3.15, 3.10, 3.07, 1.58, 1.56 ],
-        [ 3.55, 3.76, 4.02, 2.43, 3.15, 0, 2.44, 2.88, 2.11, 2.45 ],
-        [ 2.44, 2.32, 3.48, 2.92, 3.10, 2.44, 0, 0.85, 1.52, 2.00 ],
-        [ 2.87, 2.54, 3.66, 3.14, 3.07, 2.88, 0.85, 0, 1.60, 2.31 ],
-        [ 2.22, 1.92, 2.50, 1.56, 1.58, 2.11, 1.52, 1.60, 0, 0.97 ],
+      [ [ 0,    0.79, 1.63, 2.42, 2.82, 3.55, 2.44, 2.87, 2.22, 1.46 ],
+        [ 0.79, 0,    1.45, 2.32, 2.49, 3.67, 2.32, 2.54, 1.92, 1.35 ],
+        [ 1.63, 1.45, 0,    1.92, 2.09, 4.02, 3.48, 3.66, 2.50, 1.68 ],
+        [ 2.42, 2.32, 1.92, 0,    0.86, 2.43, 2.92, 3.14, 1.56, 1.14 ],
+        [ 2.82, 2.49, 2.09, 0.86, 0,    3.15, 3.10, 3.07, 1.58, 1.56 ],
+        [ 3.55, 3.76, 4.02, 2.43, 3.15, 0,    2.44, 2.88, 2.11, 2.45 ],
+        [ 2.44, 2.32, 3.48, 2.92, 3.10, 2.44, 0,    0.85, 1.52, 2.00 ],
+        [ 2.87, 2.54, 3.66, 3.14, 3.07, 2.88, 0.85, 0,    1.60, 2.31 ],
+        [ 2.22, 1.92, 2.50, 1.56, 1.58, 2.11, 1.52, 1.60, 0,    0.97 ],
         [ 1.46, 1.35, 1.68, 1.14, 1.56, 2.45, 2.00, 2.31, 0.97, 0 ] ]
       )**2
 
-X = classical_mds(EDM)
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(X[0,:], X[1,:], X[2,:])
+# Create the marker objects
+markers = tae.MarkerSet()
+markers.fromEDM(EDM, labels=labels)
 
-ax.set_xlabel('X Label')
-ax.set_ylabel('Y Label')
-ax.set_zlabel('Z Label')
+# We know that these markers should be roughly on a plane
+s = [1,2,3,6]
+print la.norm(markers.X[2,s]- markers.X[2,s].mean())
 
-plt.show()
+markers.flatten(['7','5','3','4'])
+markers.center('FPGA')
+
+markers.align('7','z')
+print markers['7']
+
+print la.norm(markers.X[2,s]- markers.X[2,s].mean())
+
+# Set the origin at 'FPGA' and x-axis 
+
+markers.plot()

test_stft_speech.py
@@ -35,7 +35,7 @@ if __name__ == '__main__':
     SNR = 0  # SNR for the received signal at microphones in [dB]
     speed_sound = pra.constants.get('c')
 
-    num_mic = 10  # number of microphones
+    num_mic = 6  # number of microphones
     K = len(speech_files)  # Real number of sources
     K_est = K  # Number of sources to estimate
 
@@ -43,9 +43,8 @@ if __name__ == '__main__':
     stop_cri = 'max_iter'  # can be 'mse' or 'max_iter'
     fft_size = 1024  # number of FFT bins
     n_bands = 4
-    fc = np.array([600, 1000, 1500, 2000])
-    fft_bins = (fc / fs * fft_size).astype(int)
-    M = 15  # Maximum Fourier coefficient index (-M to M), K_est <= M <= num_mic*(num_mic - 1) / 2
+    f_array_tuning = 600  # hertz
+    M = 14  # Maximum Fourier coefficient index (-M to M), K_est <= M <= num_mic*(num_mic - 1) / 2
 
     # Import all speech signals
     # -------------------------
@@ -65,8 +64,8 @@ if __name__ == '__main__':
 
     # Adjust the SNR
     for s in speech_signals:
-        # TODO: why speech_signals are not divided by the same sclar here?
-        s /= np.std(s)  # <= TODO: not sure why normalised by std(s)
+        # We normalize each signal so that \sum E[x_k**2] / E[ n**2 ] = SNR
+        s /= np.std(s[np.abs(s) > 1e-2]) * K
     noise_power = 10 ** (-SNR * 0.1)
 
     # ----------------------------
@@ -84,7 +83,7 @@ if __name__ == '__main__':
     print('Dirac parameter tag: ' + time_stamp)
 
     # generate microphone array layout
-    radius_array = 2.5 * speed_sound / fc[0]  # radiaus of antenna arrays
+    radius_array = 2.5 * speed_sound / f_array_tuning  # radiaus of antenna arrays
 
     # we would like gradually to switch to our "standardized" functions
     mic_array_coordinate = pra.spiral_2D_array([0, 0], num_mic,
@@ -118,7 +117,9 @@ if __name__ == '__main__':
     visi_noiseless_all = []
     for band_count in xrange(fft_bins.size):
         # Estimate the covariance matrix and extract off-diagonal entries
-        visi_noisy = extract_off_diag(cov_mtx_est(y_mic_stft[:, fft_bins[band_count], :]))
+        visi_noisy = extract_off_diag(
+                cov_mtx_est(y_mic_stft[:, fft_bins[band_count], :])
+                )
         visi_noisy_all.append(visi_noisy)
 
         visi_noiseless = extract_off_diag(cov_mtx_est(y_mic_stft_noiseless[:, fft_bins[band_count], :]))